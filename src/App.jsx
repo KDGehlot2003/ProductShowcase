20ffae83c87645195ad071acc117670ebe56240d
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-07-01 21:49:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-center items-center place-content-center">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
