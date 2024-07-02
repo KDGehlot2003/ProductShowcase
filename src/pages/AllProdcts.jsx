37cc536ab3c8351b68690542959390c87d750024
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-07-02 08:42:12 +0530

    chore: Update AllProdcts component to fetch and render product data

--- src/pages/AllProdcts.jsx
+++ src/pages/AllProdcts.jsx
@@ -5,41 +5,40 @@ import MyCard from '../components/MyCard'
 
 
 const AllProdcts = () => {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
-  useEffect( () => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://json-server.bytexl.app/products')
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json()
+    useEffect( () => {
+        const fetchData = async () => {
+        try {
+            const response = await fetch('https://json-server.bytexl.app/products')
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json()
 
-        setData(data)
-        setLoading(false)
-      } catch (error) {
-        setError(error)
-        setLoading(false)
-      }};
-      fetchData();
-  } , [])
+            setData(data)
+            setLoading(false)
+        } catch (error) {
+            setError(error)
+            setLoading(false)
+        }};
+        fetchData();
+    } , [])
 
-  
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-  
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    
 
-  return (
-    <div className='grid grid-cols-4'>
+    return (
+        <div className='grid grid-cols-4'>
 
-  {data && data.length>0 && data.map((item) => (
-    <MyCard data={item} key={item.id} />
-  ))}
-  </div>
-  )
+    {data && data.length>0 && data.map((item) => (
+        <MyCard data={item} key={item.id} />
+    ))}
+    </div>
+    )
 }
 
 export default AllProdcts
